cfbe31ab0fa51e3d80548e9bcc5a5a64451eb563
Guilherme <guilhermeboavenurarodrigues@gmail.com>
2024-03-29 22:44:32 -0300

    add .env files

--- src/main/java/com/apiempresausers/apiempresausers/ApiEmpresaUsersApplication.java
+++ src/main/java/com/apiempresausers/apiempresausers/ApiEmpresaUsersApplication.java
@@ -2,12 +2,17 @@ package com.apiempresausers.apiempresausers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication()
 public class ApiEmpresaUsersApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().load();
+		System.setProperty("DB_USER", dotenv.get("DB_USER"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
 		SpringApplication.run(ApiEmpresaUsersApplication.class, args);
 	}
 
--- src/main/java/com/apiempresausers/apiempresausers/security/WebSecurityConfig.java
+++ src/main/java/com/apiempresausers/apiempresausers/security/WebSecurityConfig.java
@@ -45,6 +45,7 @@ public class WebSecurityConfig {
                 .exceptionHandling(exception -> exception.authenticationEntryPoint(unnauthorizedHandler))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth.requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/clientes/**").permitAll()
                         .anyRequest().authenticated());
 
         http.addFilterBefore(authFilterToken(), UsernamePasswordAuthenticationFilter.class);
